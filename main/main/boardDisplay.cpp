09ab303e900f9ecd8593d1f38446a8f10d391259
BDSimeonov19 <BDSimeonov19@codingburgas.bg>
2021-10-21 23:04:22 +0300

    Change board creation algorithm

--- main/main/boardDisplay.cpp
+++ main/main/boardDisplay.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <queue>
 #include "boardDisplay.h"
 using namespace std;
 
-void displayBoard(int* board, int n) {
+void displayBoard(int* board[], int n) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			switch (board[i][j]) {
+			case 0:
+				cout << "_|";
+				break;
+			case 1:
+				cout << " |";
+				break;
+			case 2:
+				cout << "_ ";
+				break;
+			case 3:
+				cout << "  ";
+				break;
+			}
+		}
+		cout << endl;
+	}
+}
+
+void createBoard(int* path[], int n) {
 
 }
