65e5ff4cd7e76c23e670049dc7b0c64bbe3cbc54
BDSimeonov19 <BDSimeonov19@codingburgas.bg>
2021-10-21 23:04:52 +0300

    Create board displaying structure

--- main/main/boardDisplay.cpp
+++ main/main/boardDisplay.cpp
@@ -27,4 +27,38 @@ void displayBoard(int* board[], int n) {
 
 void createBoard(int* path[], int n) {
 
+	//create a dynamic matrix
+	int** board = new int* [n];
+	for (int i = 0; i < n; i++)
+		board[i] = new int[n];
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)
+			board[i][j] = 0;
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			switch (path[i][j]) {
+			case 1:
+				board[i][j] += 1;
+				break;
+			case 2:
+				board[i][j - 1] += 2;
+				break;
+			case 3:
+				board[i - 1][j] += 1;
+				break;
+			case 4:
+				board[i][j] += 2;
+				break;
+			}
+		}
+	}
+
+	displayBoard(board, n);
+
+	//delete the matrix
+	for (int i = 0; i < n; i++)
+		delete path[i];
+	delete[] path;
 }
